4975225ff87d3b647590f27d6aa5d72c33757f15
Isaac Pepió <141630285+ipepio@users.noreply.github.com>
2025-06-19 10:21:37 +0200

    Add environment config placeholder

---
+++ src/data/env_data.ts
@@ -0,0 +1,5 @@
+export const EMAIL_JS_CONFIG = {
+  serviceID: import.meta.env.VITE_EMAILJS_SERVICE_ID || '',
+  templateID: import.meta.env.VITE_EMAILJS_TEMPLATE_ID || '',
+  publicKey: import.meta.env.VITE_EMAILJS_PUBLIC_KEY || ''
+};
