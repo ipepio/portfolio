3cca5bdbf717153f5a7f3c0d64edcc18d3dd738b
Isaac <isaac@ipepio.dev>
2025-09-06 16:42:25 +0200

    Update base path configuration for production environment in Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from "vite"
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/portfolio/",
+  base: process.env.NODE_ENV === 'production' ? "/" : "/portfolio/",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
